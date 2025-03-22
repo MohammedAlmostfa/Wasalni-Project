c5dce23eee3feb56a5d81408426f6ed26528fd81
Mohammed <mohammedalmostga@gmail.com>
2025-03-22 17:00:49 +0300

    Update tripservices and bookingservices

---
+++ app/Models/SavedTrip.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SavedTrip extends Model
+{
+    use HasFactory;
+
+
+    protected $fillable=['trip_id',
+    'user_id'
+];
+
+
+
+}
